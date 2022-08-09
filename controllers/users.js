89bc9efe5cdc81135828ea81d895577b2ec8c1bd
Nanimnet <nana@markonline.ru>
2022-08-09 20:29:57 +0200

    login fix

--- controllers/users.js
+++ controllers/users.js
@@ -63,7 +63,6 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const { NODE_ENV, JWT_SECRET } = process.env;
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.cookie('token', token);
       res.send({ jwt: token });
     })
     .catch((err) => {
